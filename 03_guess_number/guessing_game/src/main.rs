6c71354879c90c38630f3c775015597cf4dafd72
Naoki Fujita <fujitanaoki0718@gmail.com>
2020-12-17 01:29:31 +0900

    feat: 数字以外を入れるとcontinueでloopの最初からやり直す

--- 03_guess_number/guessing_game/src/main.rs
+++ 03_guess_number/guessing_game/src/main.rs
@@ -11,7 +11,11 @@ fn main() {
         println!("数字を入力してね");
         let mut guess = String::new();
         std::io::stdin().read_line(&mut guess).expect("Failed to read line");  
-        let guess: u32 = guess.trim().parse().expect("Please type a number!");
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+        
         match guess.cmp(&secret_number) {
             Ordering::Less => println!("Too small!"),
             Ordering::Greater => println!("Too big!"),
