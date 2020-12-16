595083905cfdd958e174f437e2facbc2a634eeb9
Naoki Fujita <fujitanaoki0718@gmail.com>
2020-12-17 01:27:54 +0900

    feat; 数当てゲームが最低限成立するようにする

--- 03_guess_number/guessing_game/src/main.rs
+++ 03_guess_number/guessing_game/src/main.rs
@@ -1,9 +1,24 @@
-use std::io;
+extern crate rand;
+
+use rand::Rng;
+use std::cmp::Ordering;
 
 fn main() {
     println!("数当てゲーム");
-    println!("数字を入力してね");
-    let mut guess = String::new();
-    std::io::stdin().read_line(&mut guess).expect("Failed to read line");  
-    println!("あなたが入力したのは: {}", guess);
+    let secret_number = rand::thread_rng().gen_range(1, 101);
+    println!("The secret number is: {}", secret_number);  
+    loop {
+        println!("数字を入力してね");
+        let mut guess = String::new();
+        std::io::stdin().read_line(&mut guess).expect("Failed to read line");  
+        let guess: u32 = guess.trim().parse().expect("Please type a number!");
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            },
+        }
+    }
 }
