e498aad5ad6a77d856a9c1971f33f3a6e356cfdf
Naoki Fujita <fujitanaoki0718@gmail.com>
2021-11-07 16:08:16 +0900

    refact: 軽量化

--- ex3_wasm/www/index.js
+++ ex3_wasm/www/index.js
@@ -1,6 +1,6 @@
 'use strict'
 
 const wasm_modules = import('../pkg').catch(console.error);
-Promise.all([wasm_modules]).then(async function ([{ generate_mandelbrot_set, draw_mandelbrot_set }]) {
+Promise.all([wasm_modules]).then(async ([{ draw_mandelbrot_set }]) => {
     document.getElementById('render').addEventListener('click', draw_mandelbrot_set);
 });
