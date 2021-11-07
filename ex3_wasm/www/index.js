ba38ae1a353c68f003ac2538196a07385bf3f581
Naoki Fujita <fujitanaoki0718@gmail.com>
2021-11-07 16:30:15 +0900

    refact: より現代的に書く

--- ex3_wasm/www/index.js
+++ ex3_wasm/www/index.js
@@ -1,6 +1,2 @@
-'use strict'
-
-const wasm_modules = import('../pkg').catch(console.error);
-Promise.all([wasm_modules]).then(async ([{ draw_mandelbrot_set }]) => {
-    document.getElementById('render').addEventListener('click', draw_mandelbrot_set);
-});
+import { draw_mandelbrot_set } from '../pkg'
+document.getElementById('render').addEventListener('click', draw_mandelbrot_set);
